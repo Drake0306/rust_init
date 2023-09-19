8c525c241e4d6a73967d8fcd24f970173f8d7f9c
Drake0306 <danroy48@gmail.com>
2023-09-19 18:57:52 +0530

    added main

---
+++ 01/src/main.rs
@@ -0,0 +1,48 @@
+fn main() {
+    // dataTypes
+    let unsigned: u8 = 10;
+    let signed: i8 = -10;
+    let float:f32 = 1.2;
+
+    println!("Unsigned: {} Sign: {} Float: {}", unsigned, signed, float);
+
+    let letter = "c";
+    let emoji = "\u{1F600}";
+
+    println!("letter: {}, enoji: {}", letter, emoji);
+    let is_true: bool = true;
+
+    println!("isTrue: {}", is_true);
+
+
+    // Array
+    let arr:[u8; 3] = [1, 2, 3];
+    let other_arr: [u8; 5] = [100; 5];
+
+    println!("index: {}, length: {}", arr[0], other_arr.len());
+    println!("{:?}", other_arr);
+
+
+    // Tuple
+    let tuple: (u8, bool, f32) = (5, true, 2.1);
+    let tuple2 = (3, 5);
+
+    println!("first {}, second {}, third {}", tuple.0, tuple.1, tuple.2);
+    println!("{:?}", tuple2);
+
+    let (a, b, c) = tuple;
+
+    // deconstructing
+    println!("first {}, second {}, third {}", a, b, c);
+
+
+    // Function
+    println!("{}", is_even(2));
+
+}
+
+//functions
+pub fn is_even(num: u8) -> bool {
+    let digit: u8 = num % 2;
+    digit == 0 // return value
+}
