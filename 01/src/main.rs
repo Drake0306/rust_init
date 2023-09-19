81b29210dd2f7c34ce7ab608682ca51e6e1dd351
Drake0306 <danroy48@gmail.com>
2023-09-19 19:09:05 +0530

    slice and array

--- 01/src/main.rs
+++ 01/src/main.rs
@@ -39,10 +39,26 @@ fn main() {
     // Function
     println!("{}", is_even(2));
 
+
+    // slice and array
+    let arr = [0, 1, 2, 3]; // length
+    let slice = &arr[1 .. 3]; // [1, 2] don't know the length
+
+    borrowing_slice(arr, slice);
+
+
 }
 
 //functions
 pub fn is_even(num: u8) -> bool {
     let digit: u8 = num % 2;
     digit == 0 // return value
+}
+
+
+fn borrowing_slice(arr: [u8; 4], slice: &[u8]) {
+    println!("{:?}", arr);
+    println!("{:?}", slice);
+    println!("length: {}", slice.len());
+    println!("{} {}", slice[0], slice[1]);
 }
